d0d7b23f958ae9af37ecb716a06bbcd60851deb7
quinn-casey <quinn.o.casey.gr@dartmouth.edu>
2024-12-18 16:05:37 -0500

    more grids, radial/nearest selection

--- find_f.py
+++ find_f.py
@@ -7,44 +7,44 @@ def get_data_file_path(filename):
     script_dir = os.path.dirname(os.path.realpath(filename))
 
     # Construct the path to the data file relative to the script location
-    data_file_path = os.path.join(script_dir, 'f_agn/data', filename)
-    #data_file_path = os.path.join(script_dir, 'data', filename)
+    #data_file_path = os.path.join(script_dir, 'f_agn/data', filename)
+    data_file_path = os.path.join(script_dir, 'data', filename)
 
     return data_file_path
 
-dfgrid = pd.read_csv(get_data_file_path('value_table.csv'))
+# Load all grids as global variables
+grids = {
+    "r025": pd.read_csv(get_data_file_path('table_r025.csv')),
+    "r05": pd.read_csv(get_data_file_path('table_r05.csv')),
+    "r075": pd.read_csv(get_data_file_path('table_r075.csv')),
+    "r1": pd.read_csv(get_data_file_path('table_r1.csv')),
+    "n5": pd.read_csv(get_data_file_path('table_n5.csv')),
+    "n10": pd.read_csv(get_data_file_path('table_n10.csv')),
+    "n15": pd.read_csv(get_data_file_path('table_n15.csv')),
+    "n20": pd.read_csv(get_data_file_path('table_n20.csv')),
+}
 
-def kewl(x, y):
-    return y > (0.61/(x - 0.47) + 1.19)
-
-def kauf(x, y):
-    return y > (0.61/(x - 0.05) + 1.3)
-
-def kewl_line():
-    x=np.linspace(-2, 0.4, 100)
-    y = (0.61/(x - 0.47) + 1.19)
-    return x, y
-
-def calc(x, y):
+def calc(x, y, grid_name="n15"):
+    """
+    Calculate the closest grid point and return related values.
+    
+    Parameters:
+        x (float): [NII]/Ha in log space.
+        y (float): [OIII]/Hb in log space.
+        grid_name (str): Name of the grid to use (default is 'n15' which grabs the 15 nearest points). Other grids are: 'r025', 'r05', r075', 'r1', 'n5', 'n10', or 'n20'. 
+    
+    Returns:
+        pd.Series: Closest grid point values (f_mean, f_median, f_std, f_var).
+    """
+    # Get the grid by name
+    if grid_name not in grids:
+        raise ValueError(f"Grid '{grid_name}' not found. Available grids: {list(grids.keys())}")
+    
+    grid = grids[grid_name]
+    
     # Find the index of the closest x and y pair
-    index = ((dfgrid['x'] - x)**2 + (dfgrid['y'] - y)**2).idxmin()
+    index = ((grid['x'] - x)**2 + (grid['y'] - y)**2).idxmin()
     
     # Retrieve values for the closest pair
-    result = dfgrid.loc[index, ['f_mean', 'f_median', 'f_std', 'f_var']]
-    
-    # If the x, y location is outside the model, then check if the location is above/below the kewley line. If it is above then f_agn = 1 (no std/variance). Otherwise f_agn = 0 
-    if result.isna().all():
-        # AGN
-        if kewl(x, y) or (x > 0.03030):
-            result = pd.Series({'f_mean': 1, 'f_median': 1, 'f_std': 0, 'f_var': 0})
-        # Composites
-        #if (~kewl(x, y) and kauf(x, y)) or (~kewl(x, y) and (x > 0.03030)):
-        if (np.logical_not(kewl(x, y)) and kauf(x, y)) or (np.logical_not(kewl(x, y)) and (x > 0.03030)):
-            result = pd.Series({'f_mean': 0.5, 'f_median': 0.5, 'f_std': 0, 'f_var': 0})
-        if x > 0.47 and np.logical_not(kewl(x, y)):
-            result = pd.Series({'f_mean': 0.99, 'f_median': 0.99, 'f_std': 0, 'f_var': 0})
-        # SFGs
-        if np.logical_not(kauf(x, y)) and (x < 0.0303030):
-            result = pd.Series({'f_mean': 0.01, 'f_median': 0.01, 'f_std': 0, 'f_var': 0})
-    
+    result = grid.loc[index, ['f_mean', 'f_median', 'f_std', 'f_var']]
     return result
